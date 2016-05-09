728e954a8c1c206d6c0c62b60c8d0f4532c65756
Albert Wenger <albert@usv.com>
2016-05-09 07:14:39 -0400

    Update smstest.py

--- smstest.py
+++ smstest.py
@@ -7,7 +7,7 @@ TWILIO_AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
 client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN) 
  
 client.messages.create(
-    to = "+19173851022", 
-    from_ = "+16462332156", 
+    to = "your mobile number", 
+    from_ = "your twilio number", 
     body = "Person detected, view video http://embed.ziggeo.com/v1/applications/13234d4509cd5858e459a24e1494520d/videos/d75a98bf57a4a2c0cf225f057b545bd0/video.mp4", 
 )
